b03220ffa7808b7e17734f06935827758b1fb046
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-03-26 09:06:07 +0200

    add backend

---
+++ backend/src/main/java/com/ic/dinin/DininApplication.java
@@ -0,0 +1,13 @@
+package com.ic.dinin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DininApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DininApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ic/dinin/DininApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ic.dinin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DininApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
