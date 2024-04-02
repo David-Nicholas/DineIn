d5baa29ab1d85b16a49a2cca02ddedf0c0f64a5f
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-04-02 09:18:32 +0300

    add theme

---
+++ front-end/src/utils/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material/styles";
+
+
+export const DineInTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#1976d2",
+        },
+    },
+    typography: {
+        fontFamily: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ].join(','),
+      },
+      breakpoints: {
+        values: {
+          xs: 0,
+          sm: 600,
+          md: 900,
+          lg: 1200,
+          xl: 1536,
+        },
+      },   
+});
