e051714280a650caa14590cc23cf8a78b0590e32
Cocoreanu David-Nicholas <cocoreanudavid@yahoo.com>
2024-04-06 20:06:39 +0300

    midified Navbar style

--- front-end/src/app/ClientLayout.tsx
+++ front-end/src/app/ClientLayout.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
 import { DineInTheme } from "@/utils/theme";
+import Navbar from "@/components/Navbar";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider theme={DineInTheme}>
         {children}
+        <Navbar></Navbar>
     </ThemeProvider>
+    
   );
 };
 
