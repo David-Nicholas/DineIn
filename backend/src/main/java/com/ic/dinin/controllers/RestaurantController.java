47a5f4d1b7f5c84ce6b9694fea39cfccc53b30da
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-04-08 21:08:52 +0300

    #9 add get method for restaurants

--- backend/src/main/java/com/ic/dinin/controllers/RestaurantController.java
+++ backend/src/main/java/com/ic/dinin/controllers/RestaurantController.java
@@ -1,7 +1,28 @@
 package com.ic.dinin.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.ic.dinin.domain.Restaurant;
+import com.ic.dinin.repositories.RestaurantRepository;
+import com.ic.dinin.services.RestaurantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantController(RestaurantService restaurantService, RestaurantRepository restaurantRepository) {
+        this.restaurantService = restaurantService;
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @GetMapping("/restaurants")
+    public ResponseEntity<List<Restaurant>> getCategories() {
+        List<Restaurant> restaurants = restaurantService.getCategories();
+        return ResponseEntity.ok(restaurants);
+    }
 }
