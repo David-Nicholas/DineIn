d82b35467e14230f1e5cdf692a522de3a17a03cf
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-04-08 21:51:01 +0300

    #9 more refactoring

--- front-end/src/app/restaurants/map/page.tsx
+++ front-end/src/app/restaurants/map/page.tsx
@@ -6,34 +6,34 @@ import Typography from '@mui/material/Typography';
 import ConstructionIcon from '@mui/icons-material/Construction';
 
 export default function Page() {
-    return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh'
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
+      <Box sx={{
+        textAlign: 'center'
+      }}>
+        <Typography sx={{
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          fontSize: '20px',
+          lineHeight: '1.2',
+          letterSpacing: '.1rem'
         }}>
-            <Box sx={{
-                textAlign: 'center'
-            }}>
-                <Typography sx={{
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    fontSize: '20px',
-                    lineHeight: '1.2',
-                    letterSpacing: '.1rem'
-                }}>
-                  <ConstructionIcon sx={{ fontSize: 64 }}/> <br />  Coming Soon
-                </Typography>
-                <Box sx={{
-                    display: 'flex',
-                    gap: '40px',
-                    justifyContent: 'center',
-                    marginTop: '30px'
-                }}>
-                    <CustomButton text="back" route="/" />
-                </Box>
-            </Box>
+          <ConstructionIcon sx={{ fontSize: 64 }}/> <br />  Coming Soon
+        </Typography>
+        <Box sx={{
+          display: 'flex',
+          gap: '40px',
+          justifyContent: 'center',
+          marginTop: '30px'
+        }}>
+          <CustomButton text="back" route="/" />
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 }
