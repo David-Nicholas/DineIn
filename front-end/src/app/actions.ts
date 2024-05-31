372d97bffa6e7f0d29c1823b14f24494ec7c4c08
Vlad Ciobotariu <vlad.ciobotariu@ozius.solutions>
2024-05-31 03:05:04 +0300

    #23 add frontend working

---
+++ front-end/src/app/actions.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import { getRequiredVariable } from "@/api/apiClient"
+
+type MakeReservationProps = {
+  reservationDuration: number
+  startReservationTime: string
+  restaurantId: number,
+  numberOfPeople: number,
+  phoneNumber: string,
+}
+
+export async function makeReservation({reservationDuration, startReservationTime, numberOfPeople, restaurantId, phoneNumber}: MakeReservationProps){
+
+  const res = await fetch(`${getRequiredVariable('NEXT_PUBLIC_API_URL')}/reservations`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ 
+      reservationDuration: reservationDuration,
+      startReservationTime: startReservationTime,
+      restaurantId: restaurantId,
+      numberOfPeople: numberOfPeople,
+      phoneNumber: phoneNumber,
+    }),
+  })
+
+  return res.json()
+}
--- front-end/src/app/restaurants/[id]/page.tsx
+++ front-end/src/app/restaurants/[id]/page.tsx
@@ -1,49 +1,136 @@
-import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
-import CustomButton from '@/components/moleculas/CustomButton';
-import { fetchData } from '@/api/apiClient';
-import { Restaurant } from "@/app/restaurants/list/page";
+"use client"
 
-async function GetNode(nodeId: string): Promise<Restaurant> {
-    const data = await fetchData(`/restaurants/${nodeId}`);
-    return data;
-}
+import { makeReservation } from "@/app/actions";
+import { Box, Button } from "@mui/material"
+import React from "react";
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const data = await GetNode(params.id);
 
-    const [description, menuUrl, phoneNumber] = data.description.split(', ').map(part => part.trim());
+type RestaurantPageProps = {
+  params: {
+    id: string;
+  }
+};
 
-    return (
-        <Container maxWidth="sm" sx={{ textAlign: 'center', mt: 4 }}>
-            <Typography variant="h4" component="h1" gutterBottom>
-                {data.name}
-            </Typography>
+const RestaurantPage = ({params}: RestaurantPageProps) => {
 
-            <Box
-                component="img"
-                sx={{
-                    height: 400,
-                    width: 550,
-                    maxHeight: { xs: 250, md: 400 },
-                    maxWidth: { xs: 300, md: 550 },
-                    borderRadius: 2,
-                }}
-                alt={data.name}
-                src={`http://localhost:8080/images/${data.imageUrl}`}
+  const [time, setTime] = React.useState<Dayjs | null>(dayjs());
+  const [date, setDate] = React.useState<Dayjs>(dayjs());  
+
+  const [numberOfPeople, setNumberOfPeople] = React.useState<number>(2);  
+  const [reservationTime, setReservationTime] = React.useState<number>(1);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setReservationTime(JSON.parse(event.target.value));
+  };
+  
+  const handleOnClick = async () => {
+    const year = date.year();
+    const month = date.month() + 1;
+    const day = date.date();
+    const combinedDateTime = dayjs(`${date.format("YYYY-MM-DD")}T${time?.format("HH")}:00:00`);
+    // console.log(combinedDateTime.toISOString());
+    
+    const res = await makeReservation({
+      reservationDuration: reservationTime,
+      startReservationTime: combinedDateTime.toISOString(),
+      numberOfPeople: numberOfPeople,
+      restaurantId: JSON.parse(params.id),
+      phoneNumber: "0770157915",
+    })
+  }
+
+  return(
+    <>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column"
+      }}>
+        <Box sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}>
+          <Box sx={{
+            backgroundColor: "#fff",
+            color: "#000",
+            width: "fit-content"
+          }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateCalendar value={date} onChange={(newValue) => {
+                setDate(newValue)
+              }} />
+            </LocalizationProvider>
+          </Box>
+          <Box sx={{
+            backgroundColor: "#fff",
+            color: "#000",
+            width: "fit-content"
+          }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker value={time} onChange={(newValue) => setTime(newValue)} views={['hours']} />
+            </LocalizationProvider>
+          </Box>
+          <Box sx={{
+            backgroundColor: "#fff",
+            color: "#000",
+            width: "fit-content"
+          }}>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="reservation-time-label">Reservation Time</InputLabel>
+              <Select
+                labelId="reservation-time-label"
+                id="reservation-time"
+                value={reservationTime.toString()}
+                label="Reservation Time"
+                onChange={handleChange}
+              >
+                <MenuItem value={0.5}>30 min</MenuItem>
+                <MenuItem value={1.0}>1 hour</MenuItem>
+                <MenuItem value={1.5}>1.5 hours</MenuItem>
+                <MenuItem value={2.0}>2 hours</MenuItem>
+                <MenuItem value={2.5}>2.5 hours</MenuItem>
+                <MenuItem value={3.0}>3 hours</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+              backgroundColor: "#fff",
+              color: "#000",
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              type="number"
+              id="number-of-people"
+              label="NumberOfPeople"
+              value={numberOfPeople}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setNumberOfPeople(JSON.parse(event.target.value));
+              }}
             />
-
-            <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 2, mb: 2 }}>
-                <CustomButton text='BOOK NOW' route=''/>
-                <CustomButton text='CALL' route={`tel:${phoneNumber}`}/>
-                <CustomButton text='VIEW MENU' route={menuUrl}/>
-            </Box>
-
-            <Box sx={{ p: 2, border: '1px solid grey', borderRadius: 2 }}>
-                <Typography variant="body1" align="center">
-                    {description}
-                </Typography>
-            </Box>
-        </Container>
-    );
+          </Box>
+        </Box>
+        <Button onClick={handleOnClick}>
+          Click me
+        </Button>
+      </Box>
+    </>
+  )
 }
+
+export default RestaurantPage
